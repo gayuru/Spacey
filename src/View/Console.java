0742cb24318da5a967a3f6027b9bea6aa5c92265
John Warwick <johno711@live.com.au>
2019-05-04 09:59:05 +1000

    Modified Login to allow multiple program managers, and fixed user not found appearing incorrectly when multiple program managers or students

--- src/View/Console.java
+++ src/View/Console.java
@@ -14,9 +14,11 @@ import java.util.List;
 public class Console {
     private Scanner scanner = new Scanner(System.in);
     private int choice;
-    private Program computerScience = new Program("BP0964","Bachelor of Computer Science", 3);
-    private Student student = new Student("s123","John Appleseed",computerScience);
-    private ProgramManager computerSciencepm = new ProgramManager("e123","Bob",computerScience);
+    private Program computerScience = new Program("BP0964", "Bachelor of Computer Science", 3);
+    private Program informationTechnology = new Program("BP162", "Bachelor of Information Technology", 3);
+    private Student student = new Student("s123", "John Appleseed", computerScience);
+    private ProgramManager computerSciencepm = new ProgramManager("e123", "Bob", computerScience);
+    private ProgramManager informationTechnologypm = new ProgramManager("e321", "Jack", informationTechnology);
     private SchoolAdmin schoolAdmin = new SchoolAdmin("a123", "Sally");
     private CourseCoordinator courseCoordinator = new CourseCoordinator("c123", "Bob");
 
@@ -24,67 +26,91 @@ public class Console {
     private List<ProgramManager> programManager = new ArrayList<>();
     private List<Program> allPrograms = new ArrayList<>();
 
-    public void run(){
+    public void run() {
         students.add(student);
         programManager.add(computerSciencepm);
-        if(!allPrograms.contains(computerScience)) {
+        programManager.add(informationTechnologypm);
+        if (!allPrograms.contains(computerScience)) {
             allPrograms.add(computerScience);
         }
-
+        if (!allPrograms.contains(informationTechnology)) {
+            allPrograms.add(informationTechnology);
+        }
         System.out.println("Welcome User. Please log in (Enter your staff/student id) :");
 
         String userType = scanner.nextLine();
 
-        if(userType.contains("e")){
-            System.out.println("Welcome " + computerSciencepm.getUserName());
-            System.out.println("Program Manager For "+ computerSciencepm.getProgram().getProgramId()+" "+ computerSciencepm.getProgram().getProgramName()+"\n");
-            viewMenu(computerSciencepm);
-        }else if(userType.contains("s")) {
-            Student st = returnStudent(userType);
-            if(st == null) {
+        if (userType.contains("e")) {
+            ProgramManager pm = returnProgramManager(userType);
+            if (pm == null) {
                 System.exit(0);
             }
-            System.out.println("Welcome "+ st.getUserName());
-            System.out.println("Student Of "+ st.getProgram().getProgramId()+" "+ st.getProgram().getProgramName()+"\n");
-            viewMenu(st);
-        } else if(userType.contains("a")) {
-            System.out.println("Welcome School Admin "+ schoolAdmin.getUserName());
+            System.out.println("Welcome " + pm.getUserName());
+            System.out.println("Program Manager For " + pm.getProgram().getProgramId() + " " + pm.getProgram().getProgramName() + "\n");
+            programManagerMenu(pm);
+        } else if (userType.contains("s")) {
+            Student st = returnStudent(userType);
+            if (st == null) {
+                System.exit(0);
+            }
+            System.out.println("Welcome " + st.getUserName());
+            System.out.println("Student Of " + st.getProgram().getProgramId() + " " + st.getProgram().getProgramName() + "\n");
+            studentMenu(st);
+        } else if (userType.contains("a")) {
+            System.out.println("Welcome School Admin " + schoolAdmin.getUserName());
             viewMenu(schoolAdmin);
 
-        } else if(userType.contains("c")) {
-            System.out.println("Welcome Course Coordinator "+ courseCoordinator.getUserName());
+        } else if (userType.contains("c")) {
+            System.out.println("Welcome Course Coordinator " + courseCoordinator.getUserName());
             viewMenu(courseCoordinator);
         }
 
     }
 
-    private Student returnStudent(String s){
-        Student st= null;
+    @SuppressWarnings("Duplicates")
+    private Student returnStudent(String s) {
+        Student st = null;
+        Boolean userFound = false;
 
-        for(int i=0;i<students.size();++i ){
-            if(students.get(i).getUserId().equals(s)){
+        for (int i = 0; i < students.size(); ++i) {
+            if (students.get(i).getUserId().equals(s)) {
                 st = students.get(i);
-            }else{
-                System.out.println("User ID : "+ s + " not found");
+                userFound = true;
             }
         }
-
+        if (userFound = false) {
+            System.out.println("User ID : " + s + " not found");
+        }
         return st;
     }
 
+    @SuppressWarnings("Duplicates")
+    private ProgramManager returnProgramManager(String s) {
+        ProgramManager pm = null;
+        Boolean userFound = false;
+
+        for (int i = 0; i < programManager.size(); ++i) {
+            if (programManager.get(i).getUserId().equals(s)) {
+                pm = programManager.get(i);
+                userFound = true;
+            }
+
+        }
+        if (userFound = false) {
+            System.out.println("User ID : " + s + " not found");
+        }
+        return pm;
+    }
+
     private void viewMenu(User u) {
-        if (u instanceof ProgramManager) {
-            programManagerMenu();
-        } else if (u instanceof Student) {
-            studentMenu();
-        } else if (u instanceof CourseCoordinator) {
+        if (u instanceof CourseCoordinator) {
             courseCoordinatorMenu();
-        } else if(u instanceof  SchoolAdmin) {
+        } else if (u instanceof SchoolAdmin) {
             schoolAdminMenu();
         }
     }
 
-    private void programManagerMenu() {
+    private void programManagerMenu(ProgramManager pm) {
         do {
             System.out.println("1) View Program");
             System.out.println("2) Add course offerings to Program");
@@ -93,14 +119,14 @@ public class Console {
             System.out.println("Enter an option:");
             choice = Integer.parseInt(scanner.nextLine());
             if (choice == 1) {
-                if (computerSciencepm.getProgram().getNumSubjects() != 0) {
-                    computerScience.printProgramSubjects();
+                if (pm.getProgram().getNumSubjects() != 0) {
+                    pm.getProgram().printProgramSubjects();
                 } else {
                     System.out.println("Proceed to option 2, no courses added");
                 }
             } else if (choice == 2) {
                 int numOfSems = 1;
-                for (Semester sem : computerSciencepm.getProgram().getAllSemesters()) {
+                for (Semester sem : pm.getProgram().getAllSemesters()) {
                     System.out.println(numOfSems + ")" + "Semester " + sem.getSemNo() + " Year " + sem.getSemYear());
                     numOfSems++;
                 }
@@ -110,8 +136,8 @@ public class Console {
                 String courseID = scanner.nextLine();
                 System.out.println("Enter Course Name: ");
                 String courseName = scanner.nextLine();
-                if(choice <= numOfSems  && choice > 0) {
-                    computerSciencepm.addCourseOffering(new Course(courseID, courseName), computerSciencepm.getProgram().getAllSemesters().get(choice - 1));
+                if (choice <= numOfSems && choice > 0) {
+                    pm.addCourseOffering(new Course(courseID, courseName), pm.getProgram().getAllSemesters().get(choice - 1));
                 } else {
                     System.out.println("Invalid Option!");
                 }
@@ -125,18 +151,18 @@ public class Console {
         run();
     }
 
-    private void studentMenu() {
+    private void studentMenu(Student st) {
         do {
             System.out.println("1) Enrol Course\n" +
-                    "2) View Enrolled Courses\n" + "3) Return to Main Menu\n" + "0) Exit\n" +  "Please enter your choice:");
+                    "2) View Enrolled Courses\n" + "3) Return to Main Menu\n" + "0) Exit\n" + "Please enter your choice:");
 
             choice = Integer.parseInt(scanner.nextLine());
 
-            if(choice == 1) {
-                enrolStudent(student);
-            } else if(choice == 2) {
-                student.printEnrolledSubjects();
-            } else if(choice == 3) {
+            if (choice == 1) {
+                enrolStudent(st);
+            } else if (choice == 2) {
+                st.printEnrolledSubjects();
+            } else if (choice == 3) {
                 break;
             } else {
                 System.exit(0);
@@ -147,12 +173,12 @@ public class Console {
 
     private void courseCoordinatorMenu() {
         do {
-            System.out.println("1) Waive Prerequisite\n" + "2) Return to Main Menu\n" + "0) Exit\n" +  "Please enter your choice:");
+            System.out.println("1) Waive Prerequisite\n" + "2) Return to Main Menu\n" + "0) Exit\n" + "Please enter your choice:");
 
             choice = Integer.parseInt(scanner.nextLine());
 
-            if(choice == 1) {
-            } else if(choice == 2) {
+            if (choice == 1) {
+            } else if (choice == 2) {
                 break;
             } else {
                 System.exit(0);
@@ -164,53 +190,53 @@ public class Console {
     private void schoolAdminMenu() {
         do {
             System.out.println("1) Set Core Course\n" +
-                    "2) Set School Electives\n" + "3) Set Prerequisites For Subject\n" + "4) Return to Main Menu\n" + "0) Exit\n" +  "Please enter your choice:");
+                    "2) Set School Electives\n" + "3) Set Prerequisites For Subject\n" + "4) Return to Main Menu\n" + "0) Exit\n" + "Please enter your choice:");
             choice = Integer.parseInt(scanner.nextLine());
-            if(choice == 1) {
-            } else if(choice == 2) {
-            } else if(choice == 3) {
+            if (choice == 1) {
+            } else if (choice == 2) {
+            } else if (choice == 3) {
                 System.out.println("Choose a program : ");
                 int numPrograms = 1;
-                for(Program program : allPrograms) {
+                for (Program program : allPrograms) {
                     System.out.println(numPrograms + ") " + program.getProgramName());
                     numPrograms++;
                 }
                 choice = Integer.parseInt(scanner.nextLine());
-                if(choice <= numPrograms  && choice > 0) {
+                if (choice <= numPrograms && choice > 0) {
                     AbstractCourse selectedSub = null;
                     Semester selectedSem = null;
                     Program chosenProgram = allPrograms.get(choice - 1);
                     chosenProgram.printProgramSubjects();
                     System.out.println("Enter Subject ID To Set Prerequisite For Subject (Sem1Year1 Subjects Are Not Allowed To Have Prerequisites) : ");
                     String subjectID = scanner.nextLine();
-                    for(Semester sem : chosenProgram.getAllSemesters()) {
-                        if(sem.findSubject(subjectID, false) != null) {
+                    for (Semester sem : chosenProgram.getAllSemesters()) {
+                        if (sem.findSubject(subjectID, false) != null) {
                             selectedSub = sem.findSubject(subjectID, false);
                             selectedSem = sem;
                         }
                     }
-                    if(selectedSub != null) {
-                        if(selectedSub.getSubjectId().equals("s1y1")) {
+                    if (selectedSub != null) {
+                        if (selectedSub.getSubjectId().equals("s1y1")) {
                             System.out.println("Unable to Select Semester 1 Year 1 Subject");
                         } else {
                             chosenProgram.printProgramPrerequisiteChoices(selectedSem);
                             //A course can only have one prerequisite and the prerequisite can be stated as one from a group
                             System.out.println("Enter Subject ID From Prerequisite Options : ");
-                            String prerequisiteID  = scanner.nextLine();
-                            for(int i = 0; i < chosenProgram.getAllSemesters().indexOf(selectedSem); i++) {
-                                if(chosenProgram.getAllSemesters().get(i).findSubject(prerequisiteID, true) != null) {
+                            String prerequisiteID = scanner.nextLine();
+                            for (int i = 0; i < chosenProgram.getAllSemesters().indexOf(selectedSem); i++) {
+                                if (chosenProgram.getAllSemesters().get(i).findSubject(prerequisiteID, true) != null) {
                                     AbstractCourse prerequisite = chosenProgram.getAllSemesters().get(i).findSubject(prerequisiteID, true);
                                     schoolAdmin.setPrerequisites(selectedSub, prerequisite);
                                 }
                             }
                         }
                     } else {
-                        if(selectedSub == null) {
+                        if (selectedSub == null) {
                             System.out.println("No Subjects with ID Of " + subjectID);
                         }
                     }
                 }
-            } else if(choice == 4) {
+            } else if (choice == 4) {
                 break;
             } else {
                 System.exit(0);
@@ -220,10 +246,9 @@ public class Console {
     }
 
 
+    private void enrolStudent(Student student) {
 
-    private void enrolStudent(Student student){
-
-        System.out.println("\n------ Student Enrolment -------" + "\n"+ "Select a semester to enrol\n" );
+        System.out.println("\n------ Student Enrolment -------" + "\n" + "Select a semester to enrol\n");
         int numOfSems = 1;
         for (Semester sem : student.getProgram().getAllSemesters()) {
             System.out.println(numOfSems + ")" + "Semester " + sem.getSemNo() + " Year " + sem.getSemYear());
@@ -231,14 +256,14 @@ public class Console {
         }
         System.out.println("Please enter your choice:");
         choice = Integer.parseInt(scanner.nextLine());
-        if(choice <= numOfSems && choice > 0) {
-            if(student.getProgram().getAllSemesters().get(choice - 1).getNumSubjects() == 0) {
+        if (choice <= numOfSems && choice > 0) {
+            if (student.getProgram().getAllSemesters().get(choice - 1).getNumSubjects() == 0) {
                 System.out.println("No Classes Added Yet");
             } else {
-                System.out.println("\n\n"+"------ Add Classes  -------\n"+ "Select the classes you wish to enrol in\n" );
+                System.out.println("\n\n" + "------ Add Classes  -------\n" + "Select the classes you wish to enrol in\n");
                 student.getProgram().getAllSemesters().get(choice - 1).printSemesterSubjects();
                 System.out.println("\nPlease enter your choice to add a Class:");
-                int courseChoice= Integer.parseInt(scanner.nextLine());
+                int courseChoice = Integer.parseInt(scanner.nextLine());
 
                 student.enrolSubject(student.getProgram().getAllSemesters().get(choice - 1).getSemIdentifier(), student.getProgram().getAllSemesters().get(choice - 1).getSubject(courseChoice));
             }
